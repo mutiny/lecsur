19f4b72210282a23d8997471d0ec71df4482c321
Robin Ury <r.ury@tuta.io>
2017-05-07 21:28:19 -0500

    Added bundled js file to gitignore and modified public folder structure to accomodate this change. Small correction of typo preventing question removal

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/js'),
     // ??? needed for HMR (also devServer.publicPath)
     publicPath: '/'
   },
