d7c137aff8b5d1154bfa22311c656d0a32896487
cptCrunchy <jasong84@gmail.com>
2017-05-09 15:39:31 -0500

    added styling to #lex to take up 100% of viewport

---
+++ public/js/app.js
@@ -0,0 +1,132 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _sockets = __webpack_require__(1);
+
+Vue.component('question-item', {
+  template: '\n    <li>\n      {{ author }} - {{ qtext }} - {{ votes }}\n      <button v-on:click="$emit(\'remove\')">X</button>\n    </li>\n  ',
+  props: ['author', 'qtext', 'votes']
+});
+var lex = new Vue({
+  el: '#lex',
+  data: {
+    username: '', // TODO
+    newQuestionText: '',
+    questions: []
+  },
+  methods: {
+    addNewQuestion: function addNewQuestion() {
+      _sockets.Socket.ask({ author: 'testAuthor', text: this.newQuestionText });
+      this.questions.push({ author: 'testAuthor', text: this.newQuestionText, votes: 0 });
+      this.newQuestionText = '';
+    },
+    updateQuestions: function updateQuestions(newQuestions) {
+      this.questions = newQuestions;
+    }
+  }
+});
+
+_sockets.Socket.initSocket(lex.updateQuestions);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/* global io */
+var Socket = exports.Socket = {
+  socket: io(),
+  initSocket: function initSocket(callback) {
+    this.socket.on('newQuestion', callback);
+  },
+  ask: function ask(question) {
+    this.socket.emit('questionAsked', question);
+  }
+};
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(0);
+
+
+/***/ })
+/******/ ]);
